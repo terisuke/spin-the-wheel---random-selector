462f28ed5f9347b387045013bde8bf7bbfc82921
Terada Kousuke <company@cor-jp.com>
2025-08-02 00:15:02 +0900

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/spin-the-wheel---random-selector/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
